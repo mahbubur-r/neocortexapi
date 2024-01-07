07f000a8e834ac0e85e5b227d3a5cf0a0ebeb84c
Mahbubur Rahman <mahbubur-r@outlook.com>
2024-01-07 23:50:09 +0100

    Project files added to group project branch

---
+++ source/MySEProject/AnomalyDetectionSample/HTMAnomalyTesting.cs
@@ -0,0 +1,19 @@
+﻿using NeoCortexApi;
+using System;
+
+namespace AnomalyDetectionSample
+{
+    public class HTMAnomalyTesting
+    {
+
+        /// <summary>
+        /// Runs the anomaly detection experiment.
+        /// </summary>
+        public void Run()
+        {
+            // HTM model training initiated
+            HTMModeltraining myModel = new HTMModeltraining();
+            Predictor myPredictor;
+        }
+    }
+}
