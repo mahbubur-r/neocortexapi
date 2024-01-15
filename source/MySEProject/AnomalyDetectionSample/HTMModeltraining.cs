ed49c2f7582d01840e310fcc7b17871a17232276
Zihadul Islam Joni <zihadul.joni@stud.fra-uas.de>
2024-01-15 22:10:20 +0100

    Using stopwatch to calculate the total training time

--- source/MySEProject/AnomalyDetectionSample/HTMModeltraining.cs
+++ source/MySEProject/AnomalyDetectionSample/HTMModeltraining.cs
@@ -6,7 +6,17 @@ namespace AnomalyDetectionSample
 {
     public class HTMModeltraining
     {
-        //Testing
+        public void RunHTMModelLearning(string trainingfolderPath, string predictingfolderPath, out Predictor predictor)
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Starting our anomaly detection experiment!!");
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("HTM Model training initiated...................");
+            // Using stopwatch to calculate the total training time
+            Stopwatch swh = Stopwatch.StartNew();
 
-    }
+        }
 }
