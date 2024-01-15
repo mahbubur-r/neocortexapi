8f594fdc711fd5de2dc0834373b7cc9412956747
Mahbubur Rahman <mahbubur-r@outlook.com>
2024-01-15 22:05:53 +0100

    Folder directory set to location of C# files. This is the relative path.

--- source/MySEProject/AnomalyDetectionSample/HTMAnomalyTesting.cs
+++ source/MySEProject/AnomalyDetectionSample/HTMAnomalyTesting.cs
@@ -5,10 +5,19 @@ namespace AnomalyDetectionSample
 {
     public class HTMAnomalyTesting
     {
-
+        private readonly string _trainingFolderPath;
+        private readonly string _predictingFolderPath;
         /// <summary>
         /// Runs the anomaly detection experiment.
         /// </summary>
+        public HTMAnomalyTesting(string trainingFolderPath = "training", string predictingFolderPath = "predicting")
+        {
+            // Folder directory set to location of C# files. This is the relative path.
+            string projectbaseDirectory = Directory.GetParent(Directory.GetCurrentDirectory())!.Parent!.Parent!.FullName;
+            _trainingFolderPath = Path.Combine(projectbaseDirectory, trainingFolderPath);
+            _predictingFolderPath = Path.Combine(projectbaseDirectory, predictingFolderPath);
+
+        }
         public void Run()
         {
             // HTM model training initiated
