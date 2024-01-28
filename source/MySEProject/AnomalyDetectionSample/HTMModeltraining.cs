0390390d448f7e1175850ffe69cb2114b752b0bf
Zihadul Islam Joni <zihadul.joni@stud.fra-uas.de>
2024-01-28 10:29:07 +0100

    Read numerical sequences from CSV files in the specified folder containing files having prediction sequences

--- source/MySEProject/AnomalyDetectionSample/HTMModeltraining.cs
+++ source/MySEProject/AnomalyDetectionSample/HTMModeltraining.cs
@@ -18,5 +18,16 @@ namespace AnomalyDetectionSample
             // Using stopwatch to calculate the total training time
             Stopwatch swh = Stopwatch.StartNew();
 
+            // Read numerical sequences from CSV files in the specified folder containing files having training(learning) sequences
+            // CSVFileReader class can also be used for single files
+            CSVFolderReader reader = new CSVFolderReader(trainingfolderPath);
+            var sequences1 = reader.ReadFolder();
+
+            // Read numerical sequences from CSV files in the specified folder containing files having prediction sequences
+            // CSVFileReader class can also be used for single files
+            CSVFolderReader reader1 = new CSVFolderReader(predictingfolderPath);
+            var sequences2 = reader1.ReadFolder();
+        }
+
         }
 }
