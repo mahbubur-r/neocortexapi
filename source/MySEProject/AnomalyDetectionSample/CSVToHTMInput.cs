abb6c26e0b22d075367744dd3652837514e344a0
Rakibul-Islam-Shawon <rakibicenstu@gmail.com>
2024-03-16 12:43:42 +0100

    Converts a list of sequences to a dictionary of sequences for facilitating HTM Engine training.

---
+++ source/MySEProject/AnomalyDetectionSample/CSVToHTMInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AnomalyDetectionSample
+{
+    public class CSVToHTMInput
+    {
+        public Dictionary<string, List<double>> BuildHTMInput(List<List<double>> sequences)
+        {
+            Dictionary<string, List<double>> dictionary = new Dictionary<string, List<double>>();
+            for (int i = 0; i < sequences.Count; i++)
+            {         
+                string key = "S" + (i + 1);
+                List<double> value = sequences[i];
+                dictionary.Add(key, value);
+            }
+            return dictionary;
+        }
+    }
+}
