a5f4acf940c1cf1c6a04e42d6a9962645b785266
mahbubur-r <mahbubur-r@outlook.com>
2025-01-30 23:33:24 +0100

    Executes the anomaly detection experiment

--- source/MySEProject/AnomalyDetectionSample/HTMAnomalyExperiment.cs
+++ source/MySEProject/AnomalyDetectionSample/HTMAnomalyExperiment.cs
@@ -6,21 +6,29 @@ using System.Text;
 
 namespace AnomalyDetectionSample
 {
+    private readonly string _trainingCSVFolderPath;
+    private readonly string _predictingCSVFolderPath;
+    private static double _totalAccuracy = 0.0;
+    private static int _iterationCount = 0;
+    private readonly double _tolerance = 0.1;
+
     /// <summary>
-    /// This class is responsible for testing an HTM model.
-    /// CSV files from both training(learning) and predicting folders will be used for training our HTM Model.
+    /// Initializes a new instance of the HTMAnomalyExperiment class with default folder paths.
     /// </summary>
-    public class HTMAnomalyExperiment
+    /// <param name="trainingFolderPath">The path to the training folder containing CSV files.</param>
+    /// <param name="predictingFolderPath">The path to the predicting folder containing CSV files.</param>
+    public HTMAnomalyExperiment(string trainingFolderPath = "anomaly_training", string predictingFolderPath = "anomaly_predicting")
     {
+        string projectBaseDirectory = Directory.GetParent(Directory.GetCurrentDirectory())!.Parent!.Parent!.FullName;
+        _trainingCSVFolderPath = Path.Combine(projectBaseDirectory, trainingFolderPath);
+        _predictingCSVFolderPath = Path.Combine(projectBaseDirectory, predictingFolderPath);
+    }
 
-        public HTMAnomalyExperiment(string trainingFolderPath = "anomaly_training", string predictingFolderPath = "anomaly_predicting")
-        {
-            string projectBaseDirectory = Directory.GetParent(Directory.GetCurrentDirectory())!.Parent!.Parent!.FullName;
-            _trainingCSVFolderPath = Path.Combine(projectBaseDirectory, trainingFolderPath);
-            _predictingCSVFolderPath = Path.Combine(projectBaseDirectory, predictingFolderPath);
-        }
+    /// <summary>
+    /// Executes the anomaly detection experiment using the HTM model.
+    /// </summary>
 
-        public void ExecuteExperiment()
+    public void ExecuteExperiment()
         {
             HTMTrainingManager htmModel = new HTMTrainingManager();
             Predictor predictor;
